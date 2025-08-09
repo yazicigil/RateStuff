93997227a3cbb177e04d6899f24badf2cdc04e7e
yazicigil <mcyazicigil@gmail.com>
2025-08-09 16:49:38 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,12 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+  ],
   theme: { extend: {} },
   plugins: [],
 };
