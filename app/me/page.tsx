f9e3d04017435881cdae9756bf6993112cb16b71
yazicigil <mcyazicigil@gmail.com>
2025-08-11 14:59:28 +0300

    Update page.tsx

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -46,7 +46,7 @@ export default function MePage() {
   useEffect(() => {
     if (status === "unauthenticated") {
       const back = encodeURIComponent(window.location.href);
-      window.location.href = `/api/auth/signin?callbackUrl=${back}`;
+      window.location.href = `/signin`;
     }
   }, [status]);
 
