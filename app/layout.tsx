6f7b60473cc762d3aee744f4b7fcb1d992b40481
yazicigil <mcyazicigil@gmail.com>
2025-08-14 20:56:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from "next/script";
 import type { Metadata } from 'next';
 import Providers from '@/components/Providers';
 
@@ -7,6 +8,10 @@ export const metadata: Metadata = { title: 'RateStuff' };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="tr">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6558549333507218"
+     crossOrigin="anonymous"></script>
+     </head>
       <body className="min-h-screen">
         <Providers>
           {children}
