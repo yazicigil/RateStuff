39b49b38738b3bc561ef4e293fde1f6e03fb53ff
yazicigil <mcyazicigil@gmail.com>
2025-08-14 23:34:01 +0300

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,7 +13,7 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
       { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
-      { src: "/apple-touch-icon.png",       sizes: "180x180", type: "image/png", purpose: "any maskable" },
+      { src: "/apple-touch-icon.png", sizes: "180x180", type: "image/png", purpose: ["any", "maskable"] },
     ],
   };
 }
