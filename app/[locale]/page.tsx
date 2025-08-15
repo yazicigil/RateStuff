62703aea39ad035e318597804bce142b85756fc9
yazicigil <mcyazicigil@gmail.com>
2025-08-15 11:25:20 +0300

    translation pull

--- app/[locale]/page.tsx
+++
@@ -1,3 +0,0 @@
-// app/[locale]/page.tsx
-import Home from '../page'; // mevcut app/page.tsx
-export default Home;
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' }
-    ]
-  },
-  i18n: {
-    locales: ['tr', 'en'],
-    defaultLocale: 'tr'
-  }
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }, { protocol: 'http', hostname: '**' }] }
 };
-
 export default nextConfig;
