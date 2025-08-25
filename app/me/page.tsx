691b1b9b479028909eaa226415bf5fdc2a260468
yazicigil <mcyazicigil@gmail.com>
2025-08-25 16:57:23 +0300

    Update page.tsx

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,6 +1,9 @@
 'use client';
-import React from "react";
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 
+import React from "react";
 import Link from "next/link";
 import { useEffect, useMemo, useState } from "react";
 import { useCallback, useRef } from "react";
