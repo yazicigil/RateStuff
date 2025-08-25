02b96c11affefa708b0fb8a8c525ffc729f3ea39
yazicigil <mcyazicigil@gmail.com>
2025-08-25 17:00:16 +0300

    Update page.tsx

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate = false;
 export const fetchCache = 'force-no-store';
 
 import React from "react";
