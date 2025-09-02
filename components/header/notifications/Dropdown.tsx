bd20b6d34e3dec629ac2981ae748144749a92d94
yazicigil <mcyazicigil@gmail.com>
2025-09-02 14:35:03 +0300

    hop

--- components/header/notifications/Dropdown.tsx
+++ components/header/notifications/Dropdown.tsx
@@ -195,8 +195,8 @@ export default function NotificationsDropdown() {
               loop={false}
               renderer="svg"
               style={{ width: 20, height: 20 }}
-              className="bell-lottie lottie-dark-fix lottie-colorize text-neutral-800 dark:text-neutral-100"
-              rendererSettings={{ preserveAspectRatio: 'xMidYMid meet' }}
+              className="bell-lottie lottie-dark-fix"
+              rendererSettings={{ preserveAspectRatio: 'xMidYMid meet', className: 'lottie-colorize text-neutral-800 dark:text-neutral-100' }}
             />
           ) : (
             <Lottie
@@ -206,8 +206,8 @@ export default function NotificationsDropdown() {
               loop={false}
               renderer="svg"
               style={{ width: 20, height: 20 }}
-              className="bell-lottie lottie-dark-fix lottie-colorize text-neutral-800 dark:text-neutral-100"
-              rendererSettings={{ preserveAspectRatio: 'xMidYMid meet' }}
+              className="bell-lottie lottie-dark-fix"
+              rendererSettings={{ preserveAspectRatio: 'xMidYMid meet', className: 'lottie-colorize text-neutral-800 dark:text-neutral-100' }}
             />
           )}
         </span>
@@ -261,7 +261,8 @@ export default function NotificationsDropdown() {
                       loop={false}
                       renderer="svg"
                       style={{ width: 18, height: 18 }}
-                      className="refresh-lottie refresh-lottie--muted lottie-dark-fix lottie-colorize text-neutral-600 dark:text-neutral-300"
+                      className="refresh-lottie refresh-lottie--muted lottie-dark-fix"
+                      rendererSettings={{ className: 'lottie-colorize text-neutral-600 dark:text-neutral-300' }}
                     />
                   </button>
                   <button
@@ -375,7 +376,8 @@ export default function NotificationsDropdown() {
                     loop={true}
                     renderer="svg"
                     style={{ width: 32, height: 12 }}
-                    className="dots-lottie lottie-dark-fix lottie-colorize text-neutral-600 dark:text-neutral-300"
+                    className="dots-lottie lottie-dark-fix"
+                    rendererSettings={{ className: 'lottie-colorize text-neutral-600 dark:text-neutral-300' }}
                   />
                 </button>
               )}
