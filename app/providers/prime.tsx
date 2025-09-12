da02899ff7358193365e63c5d8362820cbbf6174
yazicigil <mcyazicigil@gmail.com>
2025-09-12 11:51:21 +0300

    Update globals.css

--- app/providers/prime.tsx
+++ app/providers/prime.tsx
@@ -6,12 +6,12 @@ import { PropsWithChildren } from 'react';
 export default function PrimeProvider({ children }: PropsWithChildren) {
   // SSR güvenli değer: 'self' stringi; body’ye almak istersen aşağıdaki yorumu aç
   const value = {
-    appendTo: 'self',                     // dropdown tam inputun altında
+    appendTo: typeof window !== 'undefined' ? document.body : undefined,                    // dropdown tam inputun altında
     // appendTo: typeof window !== 'undefined' ? document.body : undefined, // body’ye portal
     autoZIndex: false,                    // değerleri biz belirleyelim
     zIndex: {
       modal: 1100,
-      overlay: 60,                        // dropdown/overlay
+      overlay: 110,                        // dropdown/overlay
       menu: 60,
       tooltip: 90,
       toast: 1200,
