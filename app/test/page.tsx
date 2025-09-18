7c4ec1ba5f2bbd2f659b2a5bc5f77f8f69f6852d
yazicigil <mcyazicigil@gmail.com>
2025-09-18 19:16:52 +0300

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,1 +1,15 @@
+'use client'
+import React from "react";
+import CommentBox from "@/components/comments/CommentBox";
+import QuickAddHome from "@/components/home/QuickAddHome";
+export default function TestPage() {
+  return (
+    <div className="p-4">
+      <CommentBox itemId="cmf3q1b820002fxmv0a6sigst" />
+      <QuickAddHome onSubmit={function (payload: { name: string; desc: string; tags: string[]; rating: number; comment: string; imageUrl: string | null; productUrl: string | null; }): Promise<boolean | { ok: boolean; duplicate?: boolean; error?: string; }> | boolean | { ok: boolean; duplicate?: boolean; error?: string; } {
+              throw new Error("Function not implemented.");
+          } }></QuickAddHome>
+    </div>
+  );
+}
 
